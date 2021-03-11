e166cf40e522ab1de8328f46a5a685f4f17b8df7
Natquuu <nataliahebda7@gmail.com>
2021-03-12 00:42:25 +0100

    my message

---
+++ MvcMusicStore/Controllers/GenreMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class GenreMenuController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        //
+        // GET: /GenreMenu/
+
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.ToList();
+            return PartialView(genres);
+        }
+
+    }
+}
